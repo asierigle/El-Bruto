8a625a4a0e1456d52a2729dac2aacafce79292ee
asierigle <asier.iglesias@yahoo.es>
2016-09-13 23:36:50 +0200

    More done

--- ElBruto/Source.cpp
+++ ElBruto/Source.cpp
@@ -17,17 +17,14 @@ int main(int argc, char** argv) {
 
 	srand(time(NULL));
 
-	attack_1 = rand() % 25 + 1;
-	attack_2 = rand() % 25 + 1;
+	cout << "\t  WELCOME TO EL BRUTO" << endl;
+	cout << "\t-------------------------" << endl;
 
-	cout << "  WELCOME TO EL BRUTO" << endl;
-	cout << "-------------------------" << endl;
-
-	cout << "Enter name player 1: ";
+	cout << "\tEnter name player 1: ";
 	cin >> player1;
 	cout << endl;
 
-	cout << "Enter name player 2: ";
+	cout << "\tEnter name player 2: ";
 	cin >> player2;
 	cout << endl;
 
@@ -36,38 +33,41 @@ int main(int argc, char** argv) {
 		cin >> player2;
 	}*/
 
-	cout << "LETS START THE FIGHT!" << endl;
-	cout << "-------------------------" << endl << endl;
+	Sleep(1200);
+
+	cout << "\tLETS START THE FIGHT!" << endl;
+	cout << "\t-------------------------" << endl << endl;
 
 	while (life_1 > 1 && life_2 > 1) {
 		attack_1 = rand() % 25 + 1;
 		attack_2 = rand() % 25 + 1;
 
-		cout << "TURN " << turn << endl << endl;
+		cout << "\tTURN " << turn << endl << endl;
 
 		life_2 = life_2 - attack_1;
 		if (life_2 > 0)
 		    life_1 = life_1 - attack_2;
 
-		if(life_1 > 0)
-			cout << "This is the life of " << player1 << ": " << life_1 << endl;
+		if(life_1 >= 0)
+			cout << "\tThis is the life of " << player1 << ": " << life_1 << endl;
 		else
-			cout << "This is the life of " << player1 << ": 0" << endl;
+			cout << "\tThis is the life of " << player1 << ": 0" << endl;
 
-		if(life_2 > 0 )
-			cout << "This is the life of " << player2 << ": " << life_2 << endl << endl;
+		if(life_2 >= 0 )
+			cout << "\tThis is the life of " << player2 << ": " << life_2 << endl << endl;
 		else
-			cout << "This is the life of " << player2 << ": 0" << endl << endl;
+			cout << "\tThis is the life of " << player2 << ": 0" << endl << endl;
 		turn++;
 		Sleep(1200);
 	}
 
-	if (life_1 < 1)
-		cout << player2 << " is the winner!! I am sorry " << player1 << " :(" << endl << endl;
+	if (life_1 <= 0)
+		cout << "\t" << player2 << " is the winner!! I am sorry " << player1 << " :(" << endl << endl;
 
-	else
-		cout << player1 << " is the winner!! I am sorry "  << player2 << " :(" << endl << endl;
+	if (life_2 <= 0)
+		cout << "\t" << player1 << " is the winner!! I am sorry "  << player2 << " :(" << endl << endl;
 
+	cout << "\t";
 	system("pause");
 }
 
